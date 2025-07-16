4afdd9c927acb00ad55c9b68c54f83cd989fe06f
steynbh <HENDRIK.STEYNBERG@SASOL.COM>
2025-07-16 21:44:29 +0200

    upload replace.py

---
+++ Level 1 - Foundations of AI Engineering/M01T04 – The String and Numerical Data Type/replace.py
@@ -0,0 +1,9 @@
+sentence = "The!quick!brown!fox!jumps!over!the!lazy!dog."
+sentence = sentence.replace("!", " ")
+print(sentence)
+# Write the sentence in capital letters
+sentence = sentence.upper()
+print(sentence)
+# print the sentence in reverse
+sentence = sentence[::-1]
+print(sentence)
