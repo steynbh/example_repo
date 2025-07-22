1c5b7ed5965f88cf382078355f3398f52f3b87e1
steynbh <HENDRIK.STEYNBERG@SASOL.COM>
2025-07-22 21:22:35 +0200

    upload list_types.py

---
+++ Level 1 - Foundations of AI Engineering/M02T01 – Data Structures – The List/list_types.py
@@ -0,0 +1,8 @@
+# create a list of three of my friends names as strings
+friend_names = ["Alice", "Bob", "Charlie"]
+# print the name of the first friend in the list
+print(friend_names[0])  # Output: Alice
+# print the name of the last friend in the list
+print(friend_names[-1])  # Output: Charlie
+# print the length of the list
+print(len(friend_names))  # Output: 3
