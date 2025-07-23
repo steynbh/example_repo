10c89506dd92cfb166e1f90a0357a1b73f2b28e2
steynbh <HENDRIK.STEYNBERG@SASOL.COM>
2025-07-23 21:48:11 +0200

    upload pattern.py

---
+++ Level 1 - Foundations of AI Engineering/M02T02 – Iteration/pattern.py
@@ -0,0 +1,8 @@
+# create an arrow pattern using asterisks with a for loop
+for i in range(1, 7):
+    # print asterisks
+    print('*' * (i - 1))
+# create a reverse arrow pattern using asterisks with a for loop
+for i in range(5, 2, -1):
+    # print asterisks
+    print('*' * (i - 1))
