f097704aa4d9812507c09d3fd2ae66ab5650b366
steynbh <HENDRIK.STEYNBERG@SASOL.COM>
2025-07-24 21:04:48 +0200

    upload errors2.py

--- Level 1 - Foundations of AI Engineering/M02T03 – Defensive Programming – Error Handling/Code Files/Task files/errors2.py
+++ Level 1 - Foundations of AI Engineering/M02T03 – Defensive Programming – Error Handling/Code Files/Task files/errors2.py
@@ -2,11 +2,13 @@
  
 # There are some errors in this program. Run the program, look at the error messages, and find and fix the errors.
 
-animal = Lion
+animal = "Lion" # add parentheses to string variable
 animal_type = "cub"
 number_of_teeth = 16
 
-full_spec = "This is a {animal}. It is a {number_of_teeth} and it has {animal_type} teeth"
+full_spec = f"This is a {animal}. It is a {animal_type} and it has {number_of_teeth} teeth" 
+# logial error:change the variable names around to ensure the right output
+# run time error - add f-string formatting
 
-print full_spec
+print(full_spec) # Syntax error fixed - missing parentheses
 
