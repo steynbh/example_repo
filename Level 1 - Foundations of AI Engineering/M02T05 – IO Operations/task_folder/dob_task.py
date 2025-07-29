678f7de974b4d93ccd02f16573e4adca76b90ecd
steynbh <HENDRIK.STEYNBERG@SASOL.COM>
2025-07-29 18:38:40 +0200

    Update dob_task.py

--- Level 1 - Foundations of AI Engineering/M02T05 – IO Operations/task_folder/dob_task.py
+++ Level 1 - Foundations of AI Engineering/M02T05 – IO Operations/task_folder/dob_task.py
@@ -1,6 +1,4 @@
 # open DOB text file and read the contents
-import os
-print (os.path.dirname(__file__))
 with open('DOB.txt', 'r') as file:
     # read through each line in the file
     for line in file:
